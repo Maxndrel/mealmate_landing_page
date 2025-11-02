11fe748744d92224f5d368c3b705bc6af1d8f543
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-11-02 11:27:00 +0100

    yy

--- vite-project/src/sections/PopularMeals.jsx
+++
@@ -1,7 +0,0 @@
-import LazyImage from '../components/LazyImage';
-
-<LazyImage
-                    src="https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
-                    alt={meal.name}
-                    className="w-full h-48 object-cover"
-                  />
